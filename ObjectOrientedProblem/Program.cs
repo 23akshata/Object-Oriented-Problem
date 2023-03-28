e20129557aceec0b27e6ecbbd2bf2f525d3ea8f9
akshata <akshata2018p@gamil.com>
2023-03-28 10:51:10 +0530

    [akshata]Add:Find Right Reverse Pattern

--- ObjectOrientedProblem/Program.cs
+++ ObjectOrientedProblem/Program.cs
@@ -12,11 +12,28 @@ namespace ObjectOrientedProblem
         {
 
             Console.WriteLine("Welcome to Object Oriented Practice Problems");
-            DuplicateNumbers.CheckDuplicate();
-            UniqueElements.CheckUniqueElement();
-            FrequencyOfElements.FindFrequency();
-            FindMaxMinNumbers.FindMaximumMinimum();
-
+            Console.WriteLine("Select given options");
+            Console.WriteLine("1.Duplicate Element \n2.Unique Element\n3Frequency Of Element\n4 Find Max & Min\n5.Create Reverse Right Star Pattern ");
+            int option = Convert.ToInt16(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Duplicate Number");
+                    DuplicateNumbers.CheckDuplicate();
+                    break;
+                case 2:
+                    UniqueElements.CheckUniqueElement();
+                    break;
+                case 3:
+                    FrequencyOfElements.FindFrequency();
+                    break;
+                case 4:
+                    FindMaxMinNumbers.FindMaximumMinimum();
+                    break;
+                case 5:
+                    ReversePattern.CreateReverseRightStarPattern();
+                    break;
+            }
         }
     }
 }
---
+++ ObjectOrientedProblem/ReversePattern.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProblem
+{
+    public class ReversePattern
+    {
+        public static void CreateReverseRightStarPattern()
+        {
+            Console.WriteLine("Enter number of * input");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int row = n; row >= 1; row--)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
